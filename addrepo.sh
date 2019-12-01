36ea5258f7a5b5e87ab708f451d1fe2c7b628402
beezyfbaby <25128440+beezyfbaby@users.noreply.github.com>
2019-11-30 22:59:11 -0500

    Update addrepo.sh

--- addrepo.sh
+++ addrepo.sh
@@ -1,2 +1,4 @@
 #! /bin/sh
 sudo add-apt-repository ppa:ondrej/php -y
+wget https://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/5.6.40/xampp-linux-x64-5.6.40-1-installer.run?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fxampp%2Ffiles%2FXAMPP%2520Linux%2F5.6.40%2Fxampp-linux-x64-5.6.40-1-installer.run%2Fdownload&ts=1575172665
+sudo chmod +x xampp-linux-x64-5.6.40-1-installer.run
